07a5e4b89163e7ebf014dbf865fd9cf86b5eacb1
Vadym Yeromichev <ext-vadym.yeromichev@here.com>
2018-03-29 16:49:26 +0300

    First acceptance working test

--- pynft.py
+++ pynft.py
@@ -4,22 +4,18 @@ import pandas
 import csv
 import unittest
 
-current_dir = os.getcwd()
-files_pattern = "/*.csv"
-
-files = glob.glob(current_dir+files_pattern)
-
 def get_stat_from_file(file_name):
 	df = pandas.read_csv(file_name, delimiter="\t")
 	cpus = df['%CPU'].apply(lambda x: float(x[:-1]))
 	mems = df['RSS'].apply(lambda x: float(x[:-1]))
 	return {"cpu_max":cpus.max(), "cpu_avg":cpus.sum()/cpus.shape[0], "mem_max": mems.max(), "mem_avg": mems.sum()/mems.shape[0]}
 
-
-for file_name in files:
-	stats =	get_stat_from_file(file_name)
-	print "For file: {}".format(file_name)
-	print "CPU max: {} \nCPU avg: {}\nMEMORY max: {} \nMEMORY avg: {}\n".format(stats["cpu_max"], stats["cpu_avg"], stats["mem_max"], stats["mem_avg"])
-
 if __name__ == "__main__":
-	unittest.main()
+	current_dir = os.getcwd()
+	files_pattern = "/*.csv"
+	files = glob.glob(current_dir+files_pattern)
+	for file_name in files:
+		stats =	get_stat_from_file(file_name)
+		print "For file: {}".format(file_name)
+		print "CPU max: {} \nCPU avg: {}\nMEMORY max: {} \nMEMORY avg: {}\n".format(stats["cpu_max"], stats["cpu_avg"], stats["mem_max"], stats["mem_avg"])
+
--- pynft_test.py
+++ pynft_test.py
@@ -6,12 +6,11 @@ class nftTest(unittest.TestCase):
 		pass
 
 	def test_get_stat_from_file(self):
-		pass
-		# stat = pynft.get_stat_from_file('correct.csv')
-		# self.assertEquals(40.5, stat["cpu_max"])
-		# self.assertEquals(27.6875, stat["cpu_avg"])
-		# self.assertEquals(151.6, stat["mem_max"])
-		# self.assertEquals(116.6875, stat["mem_avg"])
+		stat = pynft.get_stat_from_file('correct.csv')
+		self.assertEquals(40.5, stat["cpu_max"])
+		self.assertEquals(27.6875, stat["cpu_avg"])
+		self.assertEquals(151.6, stat["mem_max"])
+		self.assertEquals(116.6875, stat["mem_avg"])
 
 	def test_get_stat_from_incorrect_file(self):
 		pass
